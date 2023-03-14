9206980a0e517559b8c5e9561a936c45a4c3d123
saiderrahmani <didi.mousedidi@outlook.com>
2023-03-14 01:41:31 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Mohamed";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+excitedWelcomeMessage += currentUser.toUpperCase() + "!";
+let shortGreeting = "Welcome, ";
+shortGreeting += currentUser.charAt(0) + "!";
